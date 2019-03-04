e827da98ca66cdc9c41e438595b3333a42c513c2
Shen Yi <jerry31@qq.com>
2019-03-04 14:26:44 -0800

    Update labels2csv.py

--- labels2csv.py
+++ labels2csv.py
@@ -1,17 +1,16 @@
 import os
 
-def convert(path,cls,lab):
-    out_cls = open(cls,'w')
-    out_lab = open(lab,'w')
+def convert(inpath_labels, inpath_imgs, outpath_cls, outpath_labels):
+    out_cls = open(outpath_cls,'w')
+    out_lab = open(outpath_labels,'w')
     list_cls = []
-    list_pos = []
-    g = os.walk(path)
+    g = os.walk(inpath_labels)
 
     ### generate csv file for labels
-    for root, _, file_list in g:
+    for _, _, file_list in g:
         for file_name in file_list:
-            output = root + file_name.replace('.txt','.jpg')
-            label = open(root + file_name, 'r')
+            output = inpath_imgs + file_name.replace('.txt','.jpg')
+            label = open(inpath_labels + file_name, 'r')
             num = int(label.readline())
             for i in range(num):
                 line = label.readline()
@@ -27,10 +26,11 @@ def convert(path,cls,lab):
         out_cls.write(name + ',' + str(id) + '\n')
 
 def main():
-    labels_path = 'BBOX/Labels/111/'
-    csv_cls = 'classes.csv'
-    csv_lab = 'labels.csv'
-    convert(labels_path,csv_cls,csv_lab)
+    path_labels = 'BBOX/Labels/111/'
+    path_images = 'BBOX/Images/111/'
+    csv_classes = 'classes.csv'
+    csv_labels = 'labels.csv'
+    convert(path_labels,path_images,csv_classes,csv_labels)
 
 if __name__ == '__main__':
     main()
