737d421f1d0b8b0459212951872af9cb181465ad
shenyi <shenyi.nanjing@gmail.com>
2019-03-04 17:55:24 -0800

    Update labels2csv.py

--- labels2csv.py
+++ labels2csv.py
@@ -1,24 +1,23 @@
 import os
 
-def convert(inpath_labels, inpath_imgs, outpath_cls, outpath_labels):
-    out_cls = open(outpath_cls,'w')
-    out_lab = open(outpath_labels,'w')
+def convert(path_lab,path_img,cls,lab):
+    out_cls = open(cls,'w')
+    out_lab = open(lab,'w')
     list_cls = []
-    g = os.walk(inpath_labels)
+    g = os.walk(path_lab)
 
     ### generate csv file for labels
-    for _, _, file_list in g:
+    for root, _, file_list in g:
         for file_name in file_list:
-            output = inpath_imgs + file_name.replace('.txt','.jpg')
-            label = open(inpath_labels + file_name, 'r')
+            label = open(root + file_name, 'r')
             num = int(label.readline())
-            for i in range(num):
+            for _ in range(num):
                 line = label.readline()
                 cls_name = line.split()[4]
                 if cls_name not in list_cls:
                     list_cls.append(cls_name)
-                output += ',' + line.replace('\n','').replace(' ',',')
-            out_lab.write(output+'\n')
+                output = path_img + file_name.replace('.txt','.jpg') + ',' + line.replace('\n','').replace(' ',',')
+                out_lab.write(output+'\n')
     
 
     ### generate csv file for classes                
@@ -26,11 +25,11 @@ def convert(inpath_labels, inpath_imgs, outpath_cls, outpath_labels):
         out_cls.write(name + ',' + str(id) + '\n')
 
 def main():
-    path_labels = 'BBOX/Labels/111/'
-    path_images = 'BBOX/Images/111/'
-    csv_classes = 'classes.csv'
-    csv_labels = 'labels.csv'
-    convert(path_labels,path_images,csv_classes,csv_labels)
+    path_labels = 'C:/Users/sheny/Documents/GitHub/dr-2019sp/BBOX/Labels/111/'
+    path_images = 'C:/Users/sheny/Documents/GitHub/dr-2019sp/BBOX/Images/111/'
+    csv_cls = 'classes.csv'
+    csv_lab = 'labels.csv'
+    convert(path_labels,path_images,csv_cls,csv_lab)
 
 if __name__ == '__main__':
     main()
